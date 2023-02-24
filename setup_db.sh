ab6d49f664bc7a78b75178c5da67cd826d927fb5
babidi34 <karimbaidi@gmail.com>
2023-02-25 00:41:52 +0100

    restore wp : OK

--- setup_db.sh
+++ setup_db.sh
@@ -15,4 +15,4 @@ mysql -e "CREATE DATABASE IF NOT EXISTS wordpress_db; \
 
 # Mise à jour des options de WordPress
 mysql wordpress_db < /opt/wp.sql
-#mysql wordpress_db -e "UPDATE wp_options SET option_value='' WHERE option_name='siteurl' OR option_name='home';" || mysql wordpress_db -e "UPDATE mod440_options SET option_value='' WHERE option_name='siteurl' OR option_name='home';"
+mysql wordpress_db -e "UPDATE wp_options SET option_value='https://localhost/' WHERE option_name='siteurl' OR option_name='home';" || mysql wordpress_db -e "UPDATE mod440_options SET option_value='https://localhost/' WHERE option_name='siteurl' OR option_name='home';"
