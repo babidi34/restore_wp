7a5611bfba41afd1d575601b8bc7d5846aeb5fc8
babidi34 <karimbaidi@gmail.com>
2023-02-27 12:19:19 +0100

    add SERVER_IP in update_db

--- setup_db.sh
+++ setup_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SERVER_IP=$(ip route get 1 | awk '{print $7}')
+
 # Démarrage de MySQL
 service mysql start
 
@@ -15,4 +17,4 @@ mysql -e "CREATE DATABASE IF NOT EXISTS wordpress_db; \
 
 # Mise à jour des options de WordPress
 mysql wordpress_db < /opt/$db_wp
-mysql wordpress_db -e "UPDATE wp_options SET option_value='https://localhost/' WHERE option_name='siteurl' OR option_name='home';" || mysql wordpress_db -e "UPDATE mod440_options SET option_value='https://localhost/' WHERE option_name='siteurl' OR option_name='home';"
+mysql wordpress_db -e "UPDATE wp_options SET option_value='https://$SERVER_IP/' WHERE option_name='siteurl' OR option_name='home';" || mysql wordpress_db -e "UPDATE mod440_options SET option_value='https://$SERVER_IP/' WHERE option_name='siteurl' OR option_name='home';"
