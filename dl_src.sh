1b10cf3b69723868ec7106b562b901d143c829f2
babidi34 <karimbaidi@gmail.com>
2023-06-04 00:18:39 +0200

    dl_src.sh : remove old backups

--- dl_src.sh
+++ dl_src.sh
@@ -7,14 +7,14 @@ local_path="./www/"
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
 archive_name="wp_${DATE}.tar"
 
-# Connexion SFTP et téléchargement du contenu du dossier www/
 sshpass -p $sftp_pass sftp -o StrictHostKeyChecking=no -P $port $username@$host << EOF
   get -r $remote_path $local_path
   quit
 EOF
 
-# Création de l'archive
 tar -czvf $archive_name $local_path
 
-# Suppression du dossier local
-rm -r $local_path 
+rm -r $local_path
+
+# keep 2 lasts backups, remove the others
+ls -1tr wp_*.tar | sort | head -n -2 | xargs -d '\n' rm -f --
